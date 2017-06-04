536c8e07081aba9465c982e6083c460cb98cd58a
Jon <jlpiedra90@gmail.com>
2017-06-04 11:16:34 -0400

    connection errors now propogate through callback(err, null) - middleware

--- handlers/queries.js
+++ handlers/queries.js
@@ -2,127 +2,147 @@ var pool = require('../connectors/database');
 
 exports.getTopTen = function(callback) {
 	pool.getConnection(function (err, connection) {
-		if (err) callback(err, null);
-
-		connection.query({
-			sql: 'SELECT * FROM frag ORDER BY dt DESC LIMIT 10'
-		}, function(err, result, fields) {
-			if (err) {
-				callback(err, null);
-			} else {
-				callback(null, result);	
-			};			
-		});
+		if (err) {
+			callback(err, null);
+		} else {
+			connection.query({
+				sql: 'SELECT * FROM frag ORDER BY dt DESC LIMIT 10'
+			}, function(err, result, fields) {
+				if (err) {
+					callback(err, null);
+				} else {
+					callback(null, result);	
+				};			
+			});
+		};
 	});
 };
 
 exports.getByPlayer = function(name, callback) {
 	pool.getConnection(function (err, connection) {
-		if (err) callback(err, null);
-		var lName = '%' + name + '%';
+		if (err) {
+			callback(err, null);
+		} else {
+			var lName = '%' + name + '%';
 
-		connection.query({
-			sql: 'SELECT * FROM frag WHERE `kname` LIKE ? ORDER BY dt DESC',
-			values: [lName]
-		}, function(err, result, fields) {
-			if (err) {
-				callback(err, null);
-			} else {
-				callback(null, result);
-			};
-		});
+			connection.query({
+				sql: 'SELECT * FROM frag WHERE `kname` LIKE ? ORDER BY dt DESC',
+				values: [lName]
+			}, function(err, result, fields) {
+				if (err) {
+					callback(err, null);
+				} else {
+					callback(null, result);
+				};
+			});
+		};
 	});
 };
 
 exports.getByPlayerLimit = function(name, limit, callback) {
 	pool.getConnection(function (err, connection) {
-		if (err) callback(err, null);
-		var lName = '%' + name + '%';
-		var rLimit = parseInt(limit);
+		if (err) {
+			callback(err, null);
+		} else {
+			var lName = '%' + name + '%';
+			var rLimit = parseInt(limit);
 
-		connection.query({
-			sql: 'SELECT * FROM frag WHERE `kname` LIKE ? ORDER BY dt DESC LIMIT ?',
-			values: [lName, rLimit]
-		}, function(err, result, fields) {
-			if (err) {
-				callback(err, null);
-			} else {
-				callback(null, result);
-			};
-		});
+			connection.query({
+				sql: 'SELECT * FROM frag WHERE `kname` LIKE ? ORDER BY dt DESC LIMIT ?',
+				values: [lName, rLimit]
+			}, function(err, result, fields) {
+				if (err) {
+					callback(err, null);
+				} else {
+					callback(null, result);
+				};
+			});
+		};
 	});
 };
 
 exports.getByVictim = function(name, callback) {
 	pool.getConnection(function (err, connection) {
-		if (err) callback(err, null);
-		var lName = '%' + name + '%';
+		if (err) {
+			callback(err, null);
+		} else {
+			var lName = '%' + name + '%';
 
-		connection.query({
-			sql: 'SELECT * FROM frag WHERE `vname` LIKE ? ORDER BY dt DESC',
-			values: [lName]
-		}, function(err, result, fields) {
-			if (err) {
-				callback(err, null);
-			} else {
-				callback(null, result);
-			};
-		});
+			connection.query({
+				sql: 'SELECT * FROM frag WHERE `vname` LIKE ? ORDER BY dt DESC',
+				values: [lName]
+			}, function(err, result, fields) {
+				if (err) {
+					callback(err, null);
+				} else {
+					callback(null, result);
+				};
+			});
+		};
 	});
 };
 
 exports.getByVictimLimit = function(name, limit, callback) {
 	pool.getConnection(function (err, connection) {
-		if (err) callback(err, null);
-		var lName = '%' + name + '%';
-		var rLimit = parseInt(limit);
+		if (err) {
+			callback(err, null);
+		} else {
+			var lName = '%' + name + '%';
+			var rLimit = parseInt(limit);
 
-		connection.query({
-			sql: 'SELECT * FROM frag WHERE `vname` LIKE ? ORDER BY dt DESC LIMIT ?',
-			values: [lName, rLimit]
-		}, function(err, result, fields) {
-			if (err) {
-				callback(err, null);
-			} else {
-				callback(null, result);
-			};
-		});
+			connection.query({
+				sql: 'SELECT * FROM frag WHERE `vname` LIKE ? ORDER BY dt DESC LIMIT ?',
+				values: [lName, rLimit]
+			}, function(err, result, fields) {
+				if (err) {
+					callback(err, null);
+				} else {
+					callback(null, result);
+				};
+			});
+		};
 	});
 };
 
 exports.getByWeapon = function(name, callback) {
 	pool.getConnection(function (err, connection) {
-		if (err) callback(err, null);
-		var lName = '%' + name + '%';
+		if (err) {
+			callback(err, null);
+		} else {
+			var lName = '%' + name + '%';
 
-		connection.query({
-			sql: 'SELECT * FROM frag WHERE `kwep` LIKE ? ORDER BY dt DESC',
-			values: [lName]
-		}, function(err, result, fields) {
-			if (err) {
-				callback(err, null);
-			} else {
-				callback(null, result);
-			};
-		});
+			connection.query({
+				sql: 'SELECT * FROM frag WHERE `kwep` LIKE ? ORDER BY dt DESC',
+				values: [lName]
+			}, function(err, result, fields) {
+				if (err) {
+					callback(err, null);
+				} else {
+					callback(null, result);
+				};
+			});
+		};
 	});
 };
 
 exports.getByWeaponLimit = function(name, limit, callback) {
 	pool.getConnection(function (err, connection) {
-		if (err) callback(err, null);
-		var lName = '%' + name + '%';
-		var rLimit = parseInt(limit);
+		if (err) {
+			callback(err, null);
+		} else {
+			var lName = '%' + name + '%';
+			var rLimit = parseInt(limit);
 
-		connection.query({
-			sql: 'SELECT * FROM frag WHERE `kwep` LIKE ? ORDER BY dt DESC LIMIT ?',
-			values: [lName, rLimit]
-		}, function(err, result, fields) {
-			if (err) {
-				callback(err, null);
-			} else {
-				callback(null, result);
-			};
-		});
+			connection.query({
+				sql: 'SELECT * FROM frag WHERE `kwep` LIKE ? ORDER BY dt DESC LIMIT ?',
+				values: [lName, rLimit]
+			}, function(err, result, fields) {
+				if (err) {
+					callback(err, null);
+				} else {
+					callback(null, result);
+				};
+			});
+		};
 	});
 };
