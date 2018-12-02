885e888f339648331eb9655b8e16b501802a81c5
JPZ <jpzdiabolis@gmail.com>
2018-12-02 14:08:37 -0500

    allow CORS

--- app.js
+++ app.js
@@ -14,11 +14,6 @@ var app = express();
 
 // enable CORS
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,6 +33,12 @@ if (config.env !== 'development') app.use(ajaxCheck.checkRequest);
 
 // route to api urls
 app.use('/frags', frags);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
